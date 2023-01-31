7204477c9c9d8ddf5087f54bd182782ca1af6692
Isabel Terenzi <bel.terenzi@gmail.com>
2023-01-31 20:39:51 -0300

    requisito 2

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,19 @@
+from statistics import mode
+from typing import Dict, List
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(lista: List[Dict]) -> str:
+        fab_antiga = min([product["data_de_fabricacao"] for product in lista])
+
+        val_proxima = min([product["data_de_validade"] for product in lista])
+
+        mais_produtos = mode([product["nome_da_empresa"] for product in lista])
+        # https://www.w3schools.com/python/ref_stat_mode.asp
+
+        return (
+           f"Data de fabricação mais antiga: {fab_antiga}\n"
+           f"Data de validade mais próxima: {val_proxima}\n"
+           f"Empresa com mais produtos: {mais_produtos}\n"
+        )

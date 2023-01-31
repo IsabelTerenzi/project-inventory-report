fa94a30531edea84d87cb37e14d0a63e1001c2d4
Isabel Terenzi <bel.terenzi@gmail.com>
2023-01-31 20:53:43 -0300

    requisito 3

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,20 @@
+from collections import Counter
+from typing import List, Dict
+from inventory_report.reports.simple_report import SimpleReport
+
+
 class CompleteReport:
-    pass
+    def generate(lista: List[Dict]) -> str:
+        simple_report = SimpleReport.generate(lista)
+
+        # https://realpython.com/python-counter/
+        prod_estoque = Counter(product["nome_da_empresa"] for product in lista)
+        estoque = ""
+        for empresa, quantidade in prod_estoque.items():
+            estoque += (f"- {empresa}: {quantidade}\n")
+
+        return (
+          f"{simple_report}\n"
+          f"Produtos estocados por empresa:\n"
+          f"{estoque}"
+        )
